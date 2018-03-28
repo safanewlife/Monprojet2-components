397823b030b0e1e2ed539f2fee7d073cbd56b557
safanewlife <safanewlife>
2018-03-28 15:54:33 +0100

    implemnt the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -7,17 +8,27 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductService } from './services/product.service';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
+const routes = [
+  { path: '' , component: WelcomeComponent},
+  { path: 'list' , component: ProductsListComponent},
+  { path: 'welcome' , component: NotfoundComponent}
 
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     ProductsListComponent,
-    FooterComponent
+    FooterComponent,
+    WelcomeComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot( routes )
   ],
   providers: [
   ],
